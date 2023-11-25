22492b3dc4d6cbd35544313d127b13ea7f904045
laoma89 <147708883+laoma89@users.noreply.github.com>
2023-11-26 04:09:53 +0800

    Update qqapp.js

--- douyin/qqapp.js
+++ douyin/qqapp.js
@@ -1,34 +1,40 @@
-//定义一个QQApp对象，用来封装一些方法和属性
-var QQApp = {
-    //定义一个getAppId方法，用来生成一个随机的appid
-    getAppId: function() {
-        //定义一个字符集，包含16进制的数字和字母
-        var chars = '0123456789abcdef';
-        //定义一个空字符串，用来存储生成的appid
-        var appid = '';
-        //循环16次，每次从字符集中随机选择一个字符，拼接到appid后面
-        for (var i = 0; i < 16; i++) {
-            //生成一个0到15之间的随机整数，作为字符集的索引
-            var index = Math.floor(Math.random() * 16);
-            //根据索引，从字符集中取出一个字符
-            var char = chars[index];
-            //将字符拼接到appid后面
-            appid += char;
-        }
-        //返回生成的appid
-        return appid;
-    },
-    //定义一个getQRCodeUrl方法，用来生成一个二维码的外部URL地址
-    getQRCodeUrl: function(appid) {
-        //获取当前的时间戳，单位是秒
-        var timestamp = Math.floor(Date.now() / 1000);
-        //将appid和时间戳拼接成一个字符串，然后用md5算法加密，得到一个签名
-        var sign = md5(appid + timestamp);
-        //将appid，时间戳，和签名拼接成一个查询字符串，用&符号分隔
-        var query = 'appid=' + appid + '&timestamp=' + timestamp + '&sign=' + sign;
-        //将查询字符串拼接到qqapp.top域名下的getQRCodeUrl路径后面，得到一个完整的URL地址
-        var url = 'http://qqapp.top/getQRCodeUrl?' + query;
-        //返回生成的URL地址
-        return url;
+// qqapp.js
+// A library for generating Douyin mobile QR code
+// Developed by QQApp Inc.
+
+// Generate a random appid
+function getAppId() {
+    var chars = '0123456789abcdef';
+    var appid = '';
+    for (var i = 0; i < 24; i++) {
+        appid += chars[Math.floor(Math.random() * 16)];
     }
-};
+    return appid;
+}
+
+// Get the current timestamp
+function getTimestamp() {
+    return Math.floor(Date.now() / 1000);
+}
+
+// Generate a signature by encrypting appid and timestamp
+function getSign(appid, timestamp) {
+    var key = 'qqappsecretkey'; // A secret key for encryption
+    var data = appid + timestamp; // A data string for encryption
+    var sign = ''; // A signature string for encryption
+    // A simple encryption algorithm (not secure, just for demonstration)
+    for (var i = 0; i < data.length; i++) {
+        var code = data.charCodeAt(i) ^ key.charCodeAt(i % key.length); // XOR operation
+        sign += String.fromCharCode(code); // Convert to character
+    }
+    return sign;
+}
+
+// Generate a QR code URL by concatenating appid, timestamp and sign
+function getQRCodeUrl() {
+    var appid = getAppId(); // Get a random appid
+    var timestamp = getTimestamp(); // Get the current timestamp
+    var sign = getSign(appid, timestamp); // Get a signature
+    var url = 'https://v.zzsp.eu.org/douyin/getQRCodeUrl?appid=' + appid + '&timestamp=' + timestamp + '&sign=' + sign; // Concatenate the parameters and use your own domain name
+    return url; // Return the URL
+}
