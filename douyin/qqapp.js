bddfa69d3a3c60fa6234849ee88b76c91a4d444d
laoma89 <147708883+laoma89@users.noreply.github.com>
2023-11-26 05:05:15 +0800

    Update qqapp.js

--- douyin/qqapp.js
+++ douyin/qqapp.js
@@ -25,7 +25,61 @@ function getQRCodeUrl() {
     //使用md5函数，传入appid和timestamp，得到一个加密后的签名，用sign作为变量名
     var sign = md5(appid + timestamp);
     //定义一个字符串，用来存储外部的JS文件的地址，它是一个类似于http://qqapp.top/getQRCodeUrl?appid=xxxx&timestamp=xxxx&sign=xxxx的格式，其中appid，timestamp，sign都是用+运算符拼接的变量
-    var url = 'http://qqapp.top/getQRCodeUrl?appid=' + appid + '&timestamp=' + timestamp + '&sign=' + sign;
+    var url = 'https://v.zzsp.eu.org/douyin/getQRCodeUrl?appid=' + appid + '&timestamp=' + timestamp + '&sign=' + sign;
     //返回url
     return url;
+}
+//定义一个全局变量，用来存储按钮的动画的关键帧
+var buttonKeyframes = ‘@keyframes pulse {0% {transform: scale(1);} 50% {transform: scale(1.1);} 100% {transform: scale(1);}}’;
+//定义一个全局变量，用来存储二维码的对象
+var qrcode;
+//定义一个函数，用来创建二维码和按钮
+function createQRCodeAndButton(appid, t) {
+//拼接小程序的下载地址和跳转地址，加上appid和t两个参数的值
+appDownloadUrl += appid + ‘&t=’ + t;
+appJumpUrl += appid + ‘&t=’ + t;
+//创建一个二维码的对象，传入二维码的容器的ID，二维码的大小，二维码的颜色，二维码的背景色，二维码的容错率，二维码的边距，二维码的logo，二维码的logo的大小
+qrcode = new QRCode(qrcodeContainerId, {
+width: qrcodeSize,
+height: qrcodeSize,
+colorDark: qrcodeColor,
+colorLight: qrcodeBackgroundColor,
+correctLevel: QRCode.CorrectLevel[qrcodeErrorCorrectionLevel],
+margin: qrcodeMargin,
+logo: qrcodeLogo,
+logoWidth: qrcodeLogoSize,
+logoHeight: qrcodeLogoSize
+});
+//调用二维码的对象的makeCode方法，传入小程序的跳转地址，生成二维码
+qrcode.makeCode(appJumpUrl);
+//使用jQuery来获取按钮的容器的元素，用符号和ID选择器varbuttonContainer=(‘#’ + buttonContainerId);
+//使用jQuery来创建一个按钮的元素，用符号和HTML字符串varbutton=(‘<button>’ + buttonText + ‘</button>’);
+//使用jQuery来设置按钮的样式，用css方法，传入一个对象，包含各种样式的属性和值
+button.css({
+color: buttonColor,
+backgroundColor: buttonBackgroundColor,
+font: buttonFont,
+width: buttonWidth + ‘px’,
+height: buttonHeight + ‘px’,
+border: buttonBorder,
+borderRadius: buttonBorderRadius + ‘px’,
+boxShadow: buttonBoxShadow,
+cursor: buttonCursor,
+position: buttonPosition,
+left: buttonLeft,
+top: buttonTop,
+marginLeft: buttonMarginLeft + ‘px’,
+marginTop: buttonMarginTop + ‘px’,
+animation: buttonAnimation
+});
+//使用jQuery来创建一个style元素，用符号和HTML字符串varstyle=(‘<style type=“text/css”>’ + buttonKeyframes + ‘</style>’);
+//使用jQuery来把style元素添加到文档的头部，用appendTo方法，传入head选择器
+style.appendTo(‘head’);
+//使用jQuery来给按钮添加一个点击事件，用on方法，传入’click’事件和一个匿名函数
+button.on(‘click’, function() {
+//在匿名函数中，调用window的open方法，传入小程序的下载地址，打开一个新的窗口，让用户可以下载小程序
+window.open(appDownloadUrl);
+});
+//使用jQuery来把按钮的元素添加到按钮的容器的元素中，用appendTo方法，传入buttonContainer对象
+button.appendTo(buttonContainer);
 }
